47aad1acdd4aa59f7eb53b7c6f1170260b0a651d
Oleksii Vykhrystiuk <avhrst@gmail.com>
2024-06-05 20:48:29 +0300

    train

--- configs.py
+++ configs.py
@@ -1,6 +1,6 @@
 configs = {
-    "batch_size": 32,
+    "batch_size": 128,
     "lr": 0.01,
-    "epochs": 40,
-    "wandb_key": "YOUR_WANDB_KEY",
+    "epochs": 80,
+    "wandb_key": "80b641b6e2bcd9a2efd39d578d95f8299cc15a4d",
 }
